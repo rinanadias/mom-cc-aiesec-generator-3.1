8213b2a2ead8f4286e20770fe207c54fa150efcb
rinanadias <rinanadia57@gmail.com>
2026-08-13 20:35:38 +0700

    fix chromium binary tracing

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   serverExternalPackages: ['@sparticuz/chromium'],
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/@sparticuz/chromium/bin/*'],
+  },
 };
 
 export default nextConfig;
